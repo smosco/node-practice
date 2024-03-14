b9f6676db1231125b282c4dac161df1ffb9987f0
smosco <go1ruf2tk3@gmail.com>
2024-03-14 19:37:04 +0900

    route 분리

---
+++ routes/productRoute.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const Product = require('../models/productModel');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./models/projectModel');
+const productRoute = require('./routes/productRoute');
+
 const app = express();
+
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
 
 // JSON 형식의 요청 본문(body)을 파싱하기 위해 사용
 // 클라이언트가 POST 요청을 보낼 때, JSON 형식으로 데이터를 전송하면 Express 애플리케이션이 이를 쉽게 이해하고 JavaScript 객체로 변환
@@ -12,6 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ entended: false }));
 
 //routes
+
+app.use('/api/products', productRoute);
+
 app.get('/', (req, res) => {
   res.send('Hello node api');
 });
@@ -20,73 +27,10 @@ app.get('/blog', (req, res) => {
   res.send('Blog');
 });
 
-app.post('/product', async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get('/products', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get('/products/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.put('/products/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.delete('/products/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
 mongoose
-  .connect(
-    // net/ 다음에 콜렉션 이름을 써주세요 안 쓰면 test라는 이름의 콜렉션이 생김
-    `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@nodeapi.2lpkv26.mongodb.net/?retryWrites=true&w=majority&appName=nodeapi`
-  )
+  .connect(MONGO_URL)
   .then(() => {
-    app.listen(8080, () => {
+    app.listen(PORT, () => {
       console.log('node api app is running on port 8080');
     });
     console.log('connected mongodb');
