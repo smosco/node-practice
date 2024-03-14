16dda5ea4d22b9850bbb1c1be0c9356e2fcc532d
smosco <go1ruf2tk3@gmail.com>
2024-03-14 22:27:45 +0900

    cors 미들웨어 추가해서 cors 정책 적용

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const productRoute = require('./routes/productRoute');
 const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
+const FRONTEND = process.env.FRONTEND;
 
+const corsOptions = {
+  origin: FRONTEND,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// localhost:3000으로부터의 요청만 허용, array로 여러개 허용 가능
+app.use(cors(corsOptions));
 // JSON 형식의 요청 본문(body)을 파싱하기 위해 사용
 // 클라이언트가 POST 요청을 보낼 때, JSON 형식으로 데이터를 전송하면 Express 애플리케이션이 이를 쉽게 이해하고 JavaScript 객체로 변환
 app.use(express.json());
