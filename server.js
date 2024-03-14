ea7be7aef09e9f205b2181a56bb515a2a2b23ac8
smosco <go1ruf2tk3@gmail.com>
2024-03-14 15:45:40 +0900

    connect app with database mongodb

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 //routes
@@ -6,6 +10,20 @@ app.get('/', (req, res) => {
   res.send('Hello node api');
 });
 
-app.listen(8080, () => {
-  console.log('node api app is running on port 8080');
+app.get('/blog', (req, res) => {
+  res.send('Blog');
 });
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@nodeapi.2lpkv26.mongodb.net/?retryWrites=true&w=majority&appName=nodeapi`
+  )
+  .then(() => {
+    app.listen(8080, () => {
+      console.log('node api app is running on port 8080');
+    });
+    console.log('connected mongodb');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
